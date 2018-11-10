34f70b96e8c4c4f0cb63d51c15e23066e253c190
HYP3RION <39529724+andrewfurey21@users.noreply.github.com>
2018-11-10 07:32:59 -0500

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,7 +1,21 @@
+//Fibonacci Sequence
 function Fibonacci(len){
-	let nums = [0, 1];
-	for(let i = 1; i < len; i++){
-		nums.push(nums[i] + nums[i-1]);
-	}
-	console.log(nums);
+	if (len > 0){
+		let nums = [0, 1];
+	
+		if (len > 2){
+			//len - 1 so that is is the specified length
+			for(let i = 1; i < len - 1; i++){
+				nums.push(nums[i] + nums[i-1]);
+			}
+    	} else {
+			if (len === 1){
+				nums = [0];
+            }
+    	}
+		//returns the sequence (array)
+		return nums
+		console.log(nums);
+
+    } console.error('The parameter needs to be larger than 0');
 }

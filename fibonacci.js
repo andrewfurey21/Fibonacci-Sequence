b7e13fd01c42a3a5da7b913a4428fb1aaa639c75
HYP3RION <39529724+andrewfurey21@users.noreply.github.com>
2018-11-10 07:57:59 -0500

    Update fibonacci.js
    Added a specifyFibonacci function that returns the value of the specified index.
    Added error handling
    Fixed lengthened array error

--- fibonacci.js
+++ fibonacci.js
@@ -1,4 +1,9 @@
+//Code: andrewfurey21
+
 //Fibonacci Sequence
+//You can run this in the console right now!
+
+
 function Fibonacci(len){
 	if (len > 0){
 		let nums = [0, 1];
@@ -17,5 +22,59 @@ function Fibonacci(len){
 		return nums
 		console.log(nums);
 
-    } console.error('The parameter needs to be larger than 0');
+      //added error handling		
+    } else	{
+		console.error('The parameter needs to be larger than 0');
+	}
 }
+
+
+numbers = Fibonacci(10);
+console.log(numbers);
+
+/* Expected Output
+
+0: 0
+1: 1
+2: 1
+3: 2
+4: 3
+5: 5
+6: 8
+7: 13
+8: 21
+9: 34
+
+
+*/
+
+
+function specifyFibonacci(pos){
+	if (pos >= 0){
+		let nums = [0, 1];
+	
+		
+			//len - 1 so that is is the specified length
+			for(let i = 1; i < pos + 1; i++){
+				nums.push(nums[i] + nums[i-1]);
+			}
+    	 
+
+		//returns the sequence (array)
+		return nums[pos]
+		console.log(nums[pos]);
+
+		}	else {
+
+		console.error('The parameter needs to be greater or equal to 0');
+
+        }
+}
+
+let number_10 = specifyFibonacci(10);
+
+/* Expected Output:
+
+55
+
+

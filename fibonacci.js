8e913ad737cf406c4375ee6834f3737770670008
HYP3RION <39529724+andrewfurey21@users.noreply.github.com>
2018-11-10 07:09:23 -0500

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,7 +1,11 @@
+//Fibonacci Sequence
 function Fibonacci(len){
 	let nums = [0, 1];
+	
 	for(let i = 1; i < len; i++){
 		nums.push(nums[i] + nums[i-1]);
 	}
+	//returns the sequence (array) 
+	return nums
 	console.log(nums);
 }
